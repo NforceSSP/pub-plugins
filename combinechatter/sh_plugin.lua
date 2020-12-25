8e4863beca3d746f4040144f1acea52908098708
Carson <60762562+NforceSSP@users.noreply.github.com>
2020-12-25 10:56:11 -0500

    combine chatter files

---
+++ combinechatter/sh_plugin.lua
@@ -0,0 +1,10 @@
+
+local PLUGIN = PLUGIN
+
+PLUGIN.name = "Combine Chatter"
+PLUGIN.description = "Combine have regular radio feedback."
+PLUGIN.author = "Blt950"
+
+ix.util.Include("sv_plugin.lua")
+ix.util.Include("cl_plugin.lua")
+
