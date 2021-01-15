b73c7e1ae2a43b359c74a282f44527239971613d
Carson <60762562+NforceSSP@users.noreply.github.com>
2021-01-15 01:45:01 -0500

    Create cidpup.lua

---
+++ cidpup.lua
@@ -0,0 +1,25 @@
+
+local PLUGIN = PLUGIN
+
+PLUGIN.name = "CIDPUP"
+PLUGIN.author = "Nforce"
+
+function PLUGIN:CanPlayerTakeItem(client, item)
+
+	local char = client:GetCharacter()
+	local inv = char:GetInventory()
+	local ciditem = inv:HasItem("cid")
+
+	if ciditem then
+		hasID = true
+	end
+
+	if !ciditem then
+		hasID = false
+	end
+	if hasID == true then
+		if item:GetModel() == "models/gibs/metal_gib4.mdl" then -- cid index is 51
+			return false
+		end
+	end
+end
