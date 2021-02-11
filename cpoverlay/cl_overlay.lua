fddd5b41312d5f421f5fde4e9199b7341ffae426
Carson <60762562+NforceSSP@users.noreply.github.com>
2021-02-11 13:44:45 -0500

    Add files via upload

---
+++ cpoverlay/cl_overlay.lua
@@ -0,0 +1,196 @@
+-- Personally I find this too overbearing, too much info in your face. You can use it if you want.
+
+
+surface.CreateFont("HUDSmooth", {
+    font = "Roboto",
+    size = 18,
+    antialias = true,
+    weight = 350,
+})
+
+local weps = {
+    ["#HL2_Shotgun"] = "Shotgun",
+    ["#HL2_Pistol"] = ".9mm USP-Match",
+    ["#HL2_Pulse_Rifle"] = "Standard Issue Pulse Rifle",
+    ["Stunstick"] = "Stunstick",
+    ["#HL2_SMG1"] = "Sub-Machine Gun",
+    ["#HL2_357Handgun"] = ".357 Revolver",
+    ["#HL2_Grenade"] = "Grenade"
+}
+function CombHUD()
+    
+    if !LocalPlayer():IsValid() then return end -- you can fix this yourself, it errors and i cba to find the solution because it doesn't really matter since the hud still works
+    if LocalPlayer():IsCombine() then
+        local tsin = TimedSin(.68, 200, 255, 0)
+        local area = LocalPlayer():GetArea() or "Unknown" -- fyi if you step out of an area and there's no new area this won't update
+        local tcolor = team.GetColor(LocalPlayer():Team())
+        local w = ScrW() / 2
+        local h = ScrH() / 2
+        local pos = LocalPlayer():GetPos()
+        local grid = math.Round(pos.x / 100).."/"..math.Round(pos.y / 100)
+        local weapon = LocalPlayer():GetActiveWeapon()
+        if !weapon:IsValid() then return end
+        local clip = weapon:Clip1()
+		local clipMax = weapon:GetMaxClip1()
+		local count = LocalPlayer():GetAmmoCount(weapon:GetPrimaryAmmoType())
+		local secondary = LocalPlayer():GetAmmoCount(weapon:GetSecondaryAmmoType())
+        Arm = "Unknown" -- honestly i don't know if this is necessary but /shrug
+        for k, v in pairs(weps) do
+            if tostring(LocalPlayer():GetActiveWeapon():GetPrintName()) == k then
+                Arm = v or "Unknown" -- honestly i don't know if this is necessary but /shrug
+            end
+        end
+        if LocalPlayer():Health() >= 75 then -- there's probably a more efficient way to do whatever is below but eh
+            hpCol = Color(18, 196, 18)
+        elseif LocalPlayer():Health() >= 40 then
+            hpCol = Color(255,239,17)
+        else
+            if LocalPlayer():Health() < 40 then
+                hpCol = Color(tsin, 20, 20)
+            end
+        end
+
+        if LocalPlayer():Armor() >= 70 then
+            armCol = Color(18, 196, 18)
+        elseif LocalPlayer():Armor() >= 35 then
+            armCol = Color(255,239,17)
+        else
+            if LocalPlayer():Armor() < 35 then
+                armCol = Color(223,20,20)
+            end
+        end
+        if LocalPlayer():Team() == FACTION_MPF then
+            lA = "LOCAL ASSET"
+        else
+            if LocalPlayer():Team() == FACTION_OTA then
+                lA = "OVERWATCH ASSET"
+            end
+        end
+        
+        --main square 1 (unit info)
+        surface.SetDrawColor(17, 136, 247, 150)
+        surface.DrawOutlinedRect(w-630, h - 340, 300, 180, 2)
+        surface.SetDrawColor(66, 63, 63, 120)
+        surface.DrawRect(w-630, h - 340, 300, 180)
+        draw.SimpleText("LOCAL UNIT: "..LocalPlayer():Name(), "HUDSmooth", w-620, h-310, Color(255, 255, 255), TEXT_ALIGN_LEFT, TEXT_ALIGN_LEFT)
+        draw.SimpleText(lA, "HUDSmooth", w-620, h-330, tcolor)
+        draw.SimpleText("ASSET HEALTH: "..LocalPlayer():Health(), "HUDSmooth", w-620,h-290, hpCol)
+        draw.SimpleText("ASSET ARMOR: "..LocalPlayer():Armor(), "HUDSmooth", w-620,h-270, armCol)
+        draw.SimpleText("ASSET TOKENS: "..LocalPlayer():GetCharacter():GetMoney(), "HUDSmooth", w-620, h-250)
+        surface.SetDrawColor(17, 136, 247, 150)
+        surface.DrawRect(w-628,h-225, 296, 5)
+        draw.SimpleText("BIOSIGNAL ZONE: "..area, "HUDSmooth", w-620, h-210)
+        draw.SimpleText("BIOSIGNAL GRID: "..grid, "HUDSmooth", w-620, h-190)
+        --[[ local gm = LocalPlayer():GetModel() commented out because it looks ugly as hell & is unnecessary
+        local gs = LocalPlayer():GetSkin()
+        if LocalPlayer():Team() == 3 then
+            local mpf = Material("seren/mpf.png", "smooth")......0
+            surface.SetDrawColor(255, 255, 255, 150)
+            surface.SetMaterial(mpf)
+            surface.DrawTexturedRect(w-460, h-290, 64, 64)
+        end
+        if LocalPlayer():Team() == 4 then
+            if gm == "models/combine_super_soldier.mdl" then
+                local eow = Material("seren/eow.png", "smooth")
+                surface.SetDrawColor(255, 255, 255, 150)
+                surface.SetMaterial(eow)
+                surface.DrawTexturedRect(w-460, h-290, 64, 64)
+            elseif gm == "models/combine_soldier.mdl" and gs == 0 then
+                local ows = Material("seren/ows.png", "smooth")
+                surface.SetDrawColor(255, 255, 255, 150)
+                surface.SetMaterial(ows)
+                surface.DrawTexturedRect(w-470, h-290, 96, 63)
+            else
+                local sgs = Material("seren/sgs.png", "smooth")
+                surface.SetDrawColor(255, 255, 255, 180)
+                surface.SetMaterial(sgs)
+                surface.DrawTexturedRect(w-460, h-290, 64, 64)
+            end
+        end ]]
+        --main square 2 (idle feedback)
+        --[[
+        surface.DrawOutlinedRect(w + 240, h - 330, 500, 45) -- !!this will be disturbed by health, armor, and stamina bars!!
+        surface.SetDrawColor(66, 63, 63, 120)
+        surface.DrawRect(w+240, h - 330, 500, 45)]]-- to get the feedback to align with the box requires some schema configurations by the dev. commenting this out
+
+        --main square 3 (armament info)
+        ga = LocalPlayer():GetActiveWeapon():GetClass()
+        if ga == "ix_hands" or ga == "ix_keys" or ga == "gmod_tool" or ga == "weapon_physgun" then return end
+        surface.SetDrawColor(17, 136, 247, 150)
+        surface.DrawOutlinedRect(w+280, h+280, 300, 55)
+        surface.SetDrawColor(66, 63, 63, 120)
+        surface.DrawRect(w + 280, h+280, 300, 55)
+        draw.SimpleText("ARM: "..Arm, "HUDSmooth", w + 290, h + 290)
+        draw.SimpleText("[ "..clip.." / "..clipMax.." ]", "HUDSmooth", w + 290, h + 310)
+        draw.SimpleText("[ "..count.." ]", "HUDSmooth", w + 360, h + 310)
+    end
+end 
+local direction = {
+    [0] = "N",
+    [45] = "NE",
+    [90] = "E",
+    [135] = "SE",
+    [180] = "S",
+    [225] = "SW",
+    [270] = "W",
+    [315] = "NW",
+    [360] = "N"
+}
+
+local function CombineCompass()
+    if !LocalPlayer():IsCombine() then return end
+    local ang = LocalPlayer():EyeAngles()
+    local width = ScrW() * .23
+    local m = 1
+    local spacing = (width * m) / 360
+    local lines = width / spacing
+    local rang = math.Round(ang.y)
+
+    surface.SetDrawColor(0, 0, 0, 175)
+    surface.DrawRect(ScrW() / 2 - (width / 2) - 8, 30, width + 16, 35)
+    surface.SetDrawColor(17, 136, 247, 150)
+    surface.DrawOutlinedRect(ScrW() / 2 - (width / 2) - 8, 30, width + 16, 35)
+
+    draw.SimpleText(ang, "BudgetLabel", ScrW() / 2, 50, color_white, TEXT_ALIGN_CENTER)
+
+    for i = (rang - (lines / 2)) % 360, ((rang - (lines / 2)) % 360) + lines do
+        local x = (ScrW() / 2 + (width / 2)) - ((i - ang.y - 180) % 360) * spacing
+
+        if i % 30 == 0 and i > 0 then
+            local text = direction[360 - (i % 360)] and direction[360 - (i % 360)] or 360 - (i % 360)
+
+            draw.SimpleText(text, "BudgetLabel", x, 30, color_white, TEXT_ALIGN_CENTER)
+        end
+    end
+end
+hook.Add("HUDPaint", "CHUD", CombHUD)
+hook.Add("HUDPaint", "CComp", CombineCompass)
+
+local nextmessage
+local lastmessage
+local idlemessages = {
+    "Idle connection...",
+    "Pinging loopback...",
+    "Updating biosignal coordinates...",
+    "Establishing DC link...",
+    "Checking exodus protocol status...",
+    "Sending commdata to dispatch...",
+    "Checking biosignal data...",
+    "Checking BOL list...",
+    "Purporting disp updates..."
+}
+
+hook.Add("Think", "AmbientMessages", function()
+    local lp = LocalPlayer()
+
+    if (lp:Team() == FACTION_MPF or lp:Team() == FACTION_OTA) and (nextmessage or 0) < CurTime() then
+        local message = idlemessages[math.random(1, #idlemessages)]
+
+        if message != (lastmessage or "") then
+            Schema:AddCombineDisplayMessage(message)
+            lastmessage = message
+        end
+
+        nextmessage = CurTime() + math.random(3, 4)
+    end
+end)
---
+++ cpoverlay/sh_plugin.lua
@@ -0,0 +1,8 @@
+local pl = PLUGIN
+
+pl.name = "Combine Overlay"
+pl.description = "yada yada"
+pl.author = "Nforce"
+
+ix.util.Include("cl_overlay.lua")
+
